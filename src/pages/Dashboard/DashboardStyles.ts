5da828ce50ebb38c00873b174eaf2d09820ddc67
Leonardo Silva <lorsilva.fcamara@rd.com.br>
2024-01-22 17:14:31 -0300

    atlas

---
+++ src/pages/Dashboard/DashboardStyles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+    .ok{
+        background: rgb(30 183 30 / 16%);
+    }
+
+    tbody {
+        tr{
+            transition: all linear 0.75s;
+        }
+    }
+`;
+
+export const Paragraph = styled.span`
+    background: red;
+`;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState, useRef } from "react";
-import Header from "../../components/Header";
 
+import * as S from "./DashboardStyles";
+
+import Header from "../../components/Header";
 import axios from "axios";
 import { Progress, Button, Table, Badge } from "flowbite-react";
-
 import { useToast } from "../../context/ToastContext";
 
 interface Product {
@@ -17,11 +18,13 @@ interface Product {
 }
 
 export default function Dashboard() {
-  const [data, setData] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+
   const [link, setLink] = useState("");
   const [percent, setPercent] = useState("");
   const [onUpdate, setOnUpdate] = useState(false);
   const [load, setLoad] = useState("none");
+  const [skusUpdated, setSkusUpdated] = useState<any>([]);
 
   const isMounted = useRef(false);
 
@@ -35,9 +38,9 @@ export default function Dashboard() {
 
   const fetchData = async () => {
     await axios
-      .get("https://trail-river-coriander.glitch.me/links")
+      .get("https://server-zfw1.onrender.com/links/")
       .then(({ data: response }) => {
-        setData(response);
+        setProducts(response);
       })
       .catch((err) => console.log(err));
   };
@@ -45,13 +48,19 @@ export default function Dashboard() {
   const AddLink = () => {
     setLoad("addLink");
     axios
-      .post("https://trail-river-coriander.glitch.me/save", { link: link })
+      .post("https://server-zfw1.onrender.com/links/", { link: link })
       .then(({ data }) => {
-        setData(data);
+        console.log(data);
+
+        setProducts([...products, { ...data }]);
+
         setLink("");
       })
       .catch((error: any) => {
-        alert("problema ao adicionar verifique se o link esta disponivel");
+        addToast(
+          "Problema ao adicionar, verifique se o link esta disponivel",
+          "error"
+        );
       })
       .finally(() => setLoad("none"));
   };
@@ -61,7 +70,7 @@ export default function Dashboard() {
     axios
       .delete(`https://trail-river-coriander.glitch.me/delete/${sku}`)
       .then(() => {
-        setData((data) => data.filter((item) => item.sku != sku));
+        setProducts((data) => data.filter((item) => item.sku != sku));
       })
       .catch((error) => {
         console.log("Error :", error);
@@ -71,7 +80,7 @@ export default function Dashboard() {
 
   const updateAll = () => {
     const eventSource = new EventSource(
-      "https://trail-river-coriander.glitch.me/updateAll"
+      "https://server-zfw1.onrender.com/links/update"
     );
 
     setOnUpdate(true);
@@ -82,7 +91,25 @@ export default function Dashboard() {
       if (progress.indexOf("%") != -1) {
         setPercent(progress);
       } else {
-        addToast(`${progress} Atualizado`, "success");
+        const productAtt = JSON.parse(event.data);
+
+        setSkusUpdated((skusUpdated: any) => [...skusUpdated, productAtt.sku]);
+
+        setProducts((prevProducts) => {
+          const refreshedProducts = prevProducts.map((product) => {
+            if (product.sku === productAtt.sku) {
+              return {
+                ...product,
+                nowPrice: productAtt.nowPrice,
+                lastPrice: productAtt.lastPrice,
+              };
+            }
+            return product;
+          });
+          return refreshedProducts;
+        });
+
+        addToast(`${productAtt.name} Atualizado`, "success");
       }
     };
 
@@ -91,7 +118,7 @@ export default function Dashboard() {
       eventSource.close();
       setOnUpdate(false);
       setTimeout(() => {
-        fetchData();
+        setSkusUpdated([]);
       }, 2000);
     };
   };
@@ -107,7 +134,7 @@ export default function Dashboard() {
     return `${parseFloat(diferencaPercentual.toFixed(2))}%`;
   };
 
-  if (data.length === 0) return <>Carregando</>;
+  if (products.length === 0) return <>Carregando</>;
 
   return (
     <div className="min-h-full">
@@ -132,15 +159,9 @@ export default function Dashboard() {
           )}
         </div>
       </header>
-      <main>
+      <S.Main>
         <div className="mx-auto flex gap-5 max-w-7xl py-6 sm:px-6 lg:px-8">
           <div className="sm:col-span-12">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Link
-            </label>
             <div className="mt-2">
               <input
                 style={{ padding: "5px" }}
@@ -171,7 +192,7 @@ export default function Dashboard() {
                 size="sm"
                 color="gray"
               >
-                Atualizar tudo
+                {onUpdate ? "Atualizando" : "Atualizar tudo"}
               </Button>
             </div>
           </div>
@@ -190,37 +211,39 @@ export default function Dashboard() {
                 </Table.HeadCell>
               </Table.Head>
               <Table.Body className="divide-y">
-                {data?.map((itemMl) => {
+                {products?.map((product) => {
                   return (
                     <Table.Row
-                      key={itemMl.sku}
-                      className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                      key={product.sku}
+                      className={`bg-white dark:border-gray-700 dark:bg-gray-800 ${
+                        skusUpdated.includes(product.sku) ? "ok" : ""
+                      }`}
                     >
                       <Table.Cell>
                         <img
-                          alt={itemMl.name}
-                          title={itemMl.name}
+                          alt={product.name}
+                          title={product.name}
                           style={{
                             width: "50px",
                             height: "50px",
                             objectFit: "scale-down",
                           }}
-                          src={itemMl.image}
+                          src={product.image}
                         />
                       </Table.Cell>
                       <Table.Cell
-                        title={itemMl.name}
+                        title={product.name}
                         className="whitespace-nowrap font-medium text-gray-900 dark:text-white"
                       >
-                        {itemMl.name.length > 30
-                          ? `${itemMl.name.slice(0, 30)}...`
-                          : itemMl.name}
+                        {product.name.length > 30
+                          ? `${product.name.slice(0, 30)}...`
+                          : product.name}
                       </Table.Cell>
-                      <Table.Cell>R${itemMl.nowPrice}</Table.Cell>
-                      <Table.Cell>R${itemMl.lastPrice}</Table.Cell>
+                      <Table.Cell>R${product.nowPrice}</Table.Cell>
+                      <Table.Cell>R${product.lastPrice}</Table.Cell>
                       <Table.Cell>
-                        {itemMl.status.indexOf("InStock") != -1 ||
-                        itemMl.nowPrice != 0 ? (
+                        {product.status.indexOf("InStock") != -1 ||
+                        product.nowPrice != 0 ? (
                           <Badge color="success">ON</Badge>
                         ) : (
                           <Badge color="failure">OFF</Badge>
@@ -228,13 +251,13 @@ export default function Dashboard() {
                       </Table.Cell>
                       <Table.Cell>
                         R$
-                        {itemMl.lastPrice !== itemMl.nowPrice
-                          ? (itemMl.nowPrice - itemMl.lastPrice).toFixed(2)
+                        {product.lastPrice !== product.nowPrice
+                          ? (product.nowPrice - product.lastPrice).toFixed(2)
                           : 0}
                         (
                         {calcularDiferencaPercentual(
-                          itemMl.lastPrice,
-                          itemMl.nowPrice
+                          product.lastPrice,
+                          product.nowPrice
                         )}
                         )
                       </Table.Cell>
@@ -243,7 +266,7 @@ export default function Dashboard() {
                         <a
                           href="#"
                           className="font-medium text-red-600 hover:underline dark:text-cyan-500 isProcessing"
-                          onClick={() => deleteItem(itemMl)}
+                          onClick={() => deleteItem(product)}
                         >
                           Remover
                         </a>
@@ -255,7 +278,7 @@ export default function Dashboard() {
             </Table>
           </div>
         </div>
-      </main>
+      </S.Main>
     </div>
   );
 }
